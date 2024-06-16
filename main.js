bf437437ef9e6bc315da62e3735eb3961ad470ad
arthurnunes <112280560+arthurnunesdecastro@users.noreply.github.com>
2024-06-16 16:00:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
 let dropdownMenu = document.querySelector('ul.pages')
 let menuBtn      = document.querySelector('#menu')
 let whatsappBtn  = document.getElementById('whatsapp')
+let nextBtn = document.getElementById('next');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const itemsContainer = document.querySelector('#items');
+    const items = document.querySelectorAll('.item');
+    const totalItems = items.length;
+    let index = 0;
+
+    document.querySelector('#next').addEventListener('click', () => {
+        if (index < totalItems - 1) {
+            index++;
+        } else {
+            index = 0;
+        }
+        updateCarousel();
+    });
+
+    document.querySelector('#prev').addEventListener('click', () => {
+        if (index > 0) {
+            index--;
+        } else {
+            index = totalItems - 1;
+        }
+        updateCarousel();
+    });
+
+    function updateCarousel() {
+        itemsContainer.style.transform = `translateX(-${index * 100}%)`;
+        items.forEach((item, i) => {
+            if (i === index) {
+                item.classList.add('active-item');
+            } else {
+                item.classList.remove('active-item');
+            }
+        });
+    }
+
+    // Inicializar o carrossel
+    updateCarousel();
+});
+
+let refreshInterval = setInterval(() => {
+        nextBtn.click();
+}, 30000)
+
+
+
 
 const toggleMenu = () => {
     if (dropdownMenu.className == 'pages'){
@@ -25,16 +72,26 @@ menuBtn.addEventListener('click', toggleMenu)
 
 const sendToWhatsApp = () => {
 	let number = "+554898276387";
-
+    let hour = new Date().getHours()
+    let timeofdday = ''
+    if (hour <= 12){
+        timeofdday = '*Bom Dia Dr. Felipe, vim pelo seu site.*';
+    }
+    if (hour < 17){
+        timeofdday = '*Boa Tarde Dr. Felipe, vim pelo seu site.*';
+    }
+    if (hour >= 17){
+        timeofdday = '*Boa Noite Dr. Felipe, vim pelo seu site.*';
+    }
 	let name = document.getElementById('name').value;
 	let ndapessoa = document.getElementById('number').value;
 	let message = document.getElementById('message').value;
-
-	var url = "https://wa.me/" + number + "?text="
-	+ "Oi%20meu%20nome%20é:%20" +name+ ",%20vim%20pelo%20seu%20site."+"%0a"
-	+ "N°%20de%20Telefone:%20" +ndapessoa+ "%0a"
-	+ "Mensagem:%20" + message;
-
+    let text = timeofdday+"%0a"+"%0a"
+    +"*Meu nome é:* " +name+"%0a"
+	+ "*N° de Telefone:* " +ndapessoa+ "%0a"
+	+ "*Meu caso é o seguinte:* " + message;
+    text = text.replace(/\n/g, '%0a').replace(' ', '%20');
+	var url = "https://wa.me/" + number + "?text="+text;
 	window.open(url, '_blank');
 }
 
